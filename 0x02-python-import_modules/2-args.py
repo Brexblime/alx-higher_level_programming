f0a1cab231e9b530ae912445df97072bc87567e1
Brexblime <jaouhari.bahaadine@gmail.com>
2023-05-11 14:55:20 +0100

    Write a program that prints the number of and the list of its arguments.
    The output should be:
    Number of argument(s) followed by argument (if number is one) or arguments (otherwise), followed by
    : (or . if no arguments were passed) followed by
    a new line, followed by (if at least one argument),
    one line per argument:
    the position of the argument (starting at 1) followed by :, followed by the argument value and a new line
    Your code should not be executed when imported
    The number of elements of argv can be retrieved by using: len(argv)
    You do not have to fully understand lists yet, but imagine that argv can be used just like a C array: you can use an index to walk through it. There are other ways (which will be preferred for future project tasks), if you know them you can use them.
    guillaume@ubuntu:~/0x02$ ./2-args.py
    0 arguments.
    guillaume@ubuntu:~/0x02$ ./2-args.py Hello
    1 argument:
    1: Hello
    guillaume@ubuntu:~/0x02$ ./2-args.py Hello Welcome To The Best School
    6 arguments:
    1: Hello
    2: Welcome
    3: To
    4: The
    5: Best
    6: School

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     else:
         print("{:d} arguments:".format(length))
         for i in range(1, length + 1):
-            print("{:d}: {:s}:".format(i, argv[i]))
+            print("{:d}: {:s}".format(i, argv[i]))
