ea5ee2455c483a9db85099e71cdfadef4d24d790
Brexblime <jaouhari.bahaadine@gmail.com>
2023-05-30 18:20:22 +0100

    Write a class Square that defines a square by: (based on 3-square.py)
    Private instance attribute: size:
    property def size(self): to retrieve it
    property setter def size(self, value): to set it:
    size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    if size is less than 0, raise a ValueError exception with the message size must be >= 0
    Instantiation with optional size: def __init__(self, size=0):
    Public instance method: def area(self): that returns the current square area
    You are not allowed to import any module
    Why?
    
    Why a getter and setter?
    
    Reminder: size is a private attribute. We did that to make sure we control the type and value. Getter and setter methods are not 100% Python, but more OOP. With them, you will be able to validate the assignment of a private attribute and also define how getting the attribute value will be available from outside - by copy? by assignment? etc. Also, adding type/value validation in the setter will centralize the logic, since you will do it in only one place.
    
    guillaume@ubuntu:~/0x06$ cat 4-main.py
    #!/usr/bin/python3
    Square = __import__(4-square).Square
    
    my_square = Square(89)
    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
    
    my_square.size = 3
    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
    
    try:
        my_square.size = "5 feet"
        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
    except Exception as e:
        print(e)
    
    guillaume@ubuntu:~/0x06$ ./4-main.py
    Area: 7921 for size: 89
    Area: 9 for size: 3
    size must be an integer

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""create a calss name."""
+
+
+class Square:
+    """naming it Square."""
+    def __init__(self, size=0):
+        """initilazing size."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """property to retrieve."""
+        return (self.__size)    
+    @size.setter
+    def size(self, size=0):
+        """property setter."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """defining Public instance method called area."""
+        return (self.__size ** 2)
+        """returns the current square area."""
