e2512cba1fa6633c384664144ce01d18daef87fa
Brexblime <jaouhari.bahaadine@gmail.com>
2023-06-08 01:56:47 +0100

    Write a class Rectangle that defines a rectangle by: (based on 8-rectangle.py)
    Private instance attribute: width:
    property def width(self): to retrieve it
    property setter def width(self, value): to set it:
    width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
    if width is less than 0, raise a ValueError exception with the message width must be >= 0
    Private instance attribute: height:
    property def height(self): to retrieve it
    property setter def height(self, value): to set it:
    height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
    if height is less than 0, raise a ValueError exception with the message height must be >= 0
    Public class attribute number_of_instances:
    Initialized to 0
    Incremented during each new instance instantiation
    Decremented during each instance deletion
    Public class attribute print_symbol:
    Initialized to #
    Used as symbol for string representation
    Can be any type
    Instantiation with optional width and height: def __init__(self, width=0, height=0):
    Public instance method: def area(self): that returns the rectangle area
    Public instance method: def perimeter(self): that returns the rectangle perimeter:
    if width or height is equal to 0, perimeter has to be equal to 0
    print() and str() should print the rectangle with the character #:
    if width or height is equal to 0, return an empty string
    repr() should return a string representation of the rectangle to be able to recreate a new instance by using eval()
    Print the message Bye rectangle... (... being 3 dots not ellipsis) when an instance of Rectangle is deleted
    Static method def bigger_or_equal(rect_1, rect_2): that returns the biggest rectangle based on the area
    rect_1 must be an instance of Rectangle, otherwise raise a TypeError exception with the message rect_1 must be an instance of Rectangle
    rect_2 must be an instance of Rectangle, otherwise raise a TypeError exception with the message rect_2 must be an instance of Rectangle
    Returns rect_1 if both have the same area value
    Class method def square(cls, size=0): that returns a new Rectangle instance with width == height == size
    You are not allowed to import any module
    guillaume@ubuntu:~/0x08$ cat 9-main.py
    #!/usr/bin/python3
    Rectangle = __import__(9-rectangle).Rectangle
    
    my_square = Rectangle.square(5)
    print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
    print(my_square)
    
    guillaume@ubuntu:~/0x08$ ./9-main.py
    Area: 25 - Perimeter: 20
    #####
    #####
    #####
    #####
    #####
    Bye rectangle...

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
         """
         Initializes a rectangle.
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
