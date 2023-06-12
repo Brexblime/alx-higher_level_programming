4cc2484dd3ff189dc5746b5aa1ee83a876685818
Brexblime <jaouhari.bahaadine@gmail.com>
2023-06-12 23:37:57 +0100

    update

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,30 +4,22 @@
 
 class BaseGeometry:
     """naming it BaseGeometry."""
-
-
     def area(self):
         """defining area."""
-
-
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """validates value."""
-
-
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """defining Rectangle."""
-
-
     def __init__(self, width, height):
         """initialize Rectangle instance"""
-
         self.__width = 0
         self.__height = 0
         self.integer_validator("width", width)
