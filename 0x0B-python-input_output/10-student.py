99bcce7147470d5eeaabb21e28c8682b2ee4de6d
Brexblime <jaouhari.bahaadine@gmail.com>
2023-06-14 23:37:46 +0100

    Write a class Student that defines a student by: (based on 9-student.py)
    Public instance attributes:
    first_name
    last_name
    age
    Instantiation with first_name, last_name and age: def __init__(self, first_name, last_name, age):
    Public method def to_json(self, attrs=None): that retrieves a dictionary representation of a Student instance (same as 8-class_to_json.py):
    If attrs is a list of strings, only attribute names contained in this list must be retrieved.
    Otherwise, all attributes must be retrieved
    You are not allowed to import any module
    guillaume@ubuntu:~/0x0B$ cat 10-main.py
    #!/usr/bin/python3
    Student = __import__(10-student).Student
    
    student_1 = Student("John", "Doe", 23)
    student_2 = Student("Bob", "Dylan", 27)
    
    j_student_1 = student_1.to_json()
    j_student_2 = student_2.to_json([first_name, age])
    j_student_3 = student_2.to_json([middle_name, age])
    
    print(j_student_1)
    print(j_student_2)
    print(j_student_3)
    
    guillaume@ubuntu:~/0x0B$ ./10-main.py
    {age: 23, last_name: Doe, first_name: John}
    {age: 27, first_name: Bob}
    {age: 27}

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""defines the class Student"""
+
+
+class Student:
+    """class to create student instances"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            json_dict = self.__dict__
+        else:
+            json_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    json_dict[attr] = getattr(self, attr)
+        return (json_dict)
