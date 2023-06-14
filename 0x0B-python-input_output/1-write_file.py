72aaedc6990e3106b278bdc4b99767e502d2e78c
Brexblime <jaouhari.bahaadine@gmail.com>
2023-06-14 19:26:53 +0100

    Write a function that writes a string to a text file (UTF8) and returns the number of characters written:
    Prototype: def write_file(filename=, text=):
    You must use the with statement
    You don’t need to manage file permission exceptions.
    Your function should create the file if doesn’t exist.
    Your function should overwrite the content of the file if it already exists.
    You are not allowed to import any module
    guillaume@ubuntu:~/0x0B$ cat 1-main.py
    write_file = __import__('1-write_file').write_file
    
    print(nb_characters)
    
    guillaume@ubuntu:~/0x0B$ ./1-main.py
    29
    guillaume@ubuntu:~/0x0B$ cat my_first_file.txt
    This School is so cool!

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,6 +7,7 @@ def write_file(filename="", text=""):
     writes a string to a text file (UTF8) 
     and returns the number of characters written
     """
+    char_count = 0
     with open(filename, mode='w', encoding='utf-8') as file:
         char_count = file.write(text)
     return (char_count)
