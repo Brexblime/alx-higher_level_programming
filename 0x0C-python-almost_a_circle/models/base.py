a7f4e75e55bdaf448e9fe234d5e5ca6a9dc45ae7
Brexblime <jaouhari.bahaadine@gmail.com>
2023-06-15 19:55:35 +0100

    Write the first class Base:
    Create a folder named models with an empty file __init__.py inside - with this file, the folder will become a Python package
    
    Create a file named models/base.py:
    
    Class Base:
    private class attribute __nb_objects = 0
    class constructor: def __init__(self, id=None)::
    if id is not None, assign the public instance attribute id with this argument value - you can assume id is an integer and you don’t need to test the type of it
    otherwise, increment __nb_objects and assign the new value to the public instance attribute id
    This class will be the “base” of all other classes in this project. The goal of it is to manage id attribute in all your future classes and to avoid duplicating the same code (by extension, same bugs)
    
    guillaume@ubuntu:~/$ cat 0-main.py
    #!/usr/bin/python3
    """ 0-main """
    from models.base import Base
    
    if __name__ == "__main__":
    
        b1 = Base()
        print(b1.id)
    
        b2 = Base()
        print(b2.id)
    
        b3 = Base()
        print(b3.id)
    
        b4 = Base(12)
        print(b4.id)
    
        b5 = Base()
        print(b5.id)
    
    guillaume@ubuntu:~/$ ./0-main.py
    1
    2
    3
    12
    4

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains the Base class.
+"""
+
+
+class Base:
+    """
+    The Base class.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initializes a Base instance.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
