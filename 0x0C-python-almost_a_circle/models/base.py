962435924da266606995e020f88ed3455c770a02
Brexblime <jaouhari.bahaadine@gmail.com>
2023-06-20 00:45:57 +0100

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """ Module containing the Base class """
 
 
+import json
+
+
 class Base:
     """ Base class for managing id attribute """
     __nb_objects = 0
@@ -16,8 +19,15 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ returns JSON string representation of list_dictionaries"""
+        """ Returns the JSON string representation of list_dictionaries """
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return ("[]")
-        else:
-            return (json.dumps(list_dictionaries))
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes the JSON string representation of list_objs to a file """
+        filename = cls.__name__ + ".json"
+        json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+        with open(filename, "w") as file:
+            file.write(json_string)
