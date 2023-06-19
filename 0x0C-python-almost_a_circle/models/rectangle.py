f623eeba5a1f0d0125a0669c96077081bdb4201f
Brexblime <jaouhari.bahaadine@gmail.com>
2023-06-20 00:18:32 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,16 @@ class Rectangle(Base):
         """ Returns a string representation of the Rectangle """
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.__x, self.__y, self.__width, self.__height))
+
+    def update(self, *args):
+        """ Updates the attributes of the Rectangle """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
