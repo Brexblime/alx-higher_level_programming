d003cbc1ca12424585486abd6a79738206f7ed32
Brexblime <jaouhari.bahaadine@gmail.com>
2023-06-20 00:01:00 +0100

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,58 +1,14 @@
 #!/usr/bin/python3
-"""
-This module contains the Base class.
-"""
-
-
-import json
-
+""" Module containing the Base class """
 
 class Base:
-    """
-    The Base class.
-    """
-
+    """ Base class for managing id attribute """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        Initializes a Base instance.
-        """
+        """ Initializes an instance of the Base class """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """
-        returns the JSON string representation of list_dictionaries.
-        """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return ("[]")
-        else:
-            return (json.dumps(list_dictionaries))
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """
-        writes the JSON string representation of list_objs to a file.
-        """
-        if list_objs is None:
-            list_objs = []
-        filename = cls.__name__ + ".json"
-        json_list = [obj.to_dictionary() for obj in list_objs]
-        json_string = cls.to_json_string(json_list)
-        with open(filename, "w") as file:
-            file.write(json_string)
-
-    @staticmethod
-    def from_json_string(json_string):
-        """
-        returns the list of the JSON string representation json_string.
-        """
-        if json_string is None or len(json_string) == 0:
-            return ([])
-        else:
-            return (json.loads(json_string))
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,54 @@
 #!/usr/bin/python3
-""" Module containing the Base class """
+""" Module containing the Rectangle class """
 
-class Base:
-    """ Base class for managing id attribute """
-    __nb_objects = 0
+from models.base import Base
 
-    def __init__(self, id=None):
-        """ Initializes an instance of the Base class """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+class Rectangle(Base):
+    """ Rectangle class that inherits from Base """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initializes an instance of the Rectangle class """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Getter method for width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter method for width """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter method for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter method for height """
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter method for x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter method for x """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter method for y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter method for y """
+        self.__y = value
