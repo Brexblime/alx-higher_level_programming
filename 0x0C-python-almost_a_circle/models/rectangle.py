b6e9c9e3285823c4b44a976c3a9b8dc4ac5dbeff
Brexblime <jaouhari.bahaadine@gmail.com>
2023-06-19 23:58:20 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,88 +1,14 @@
-#!/usr/bin/python4
-"""
-This module contains the Rectangle class.
-"""
+#!/usr/bin/python3
+""" Module containing the Base class """
 
+class Base:
+    """ Base class for managing id attribute """
+    __nb_objects = 0
 
-from models.base import Base
-
-class Rectangle(Base):
-    """
-    The Rectangle class, inheriting from Base.
-    """
-
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Initializes a Rectangle instance.
-        """
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    def to_dictionary(self):
-        """returns the dictionary representation of a Rectangle."""
-        attrs = ["y", "x", "id", "width", "height"]
-        return ({attr: getattr(self, attr) for attr in attrs})
-
-    @property
-    def width(self):
-        """
-        Getter for the width attribute.
-        """
-        return (self.__width)
-
-    @width.setter
-    def width(self, value):
-        """
-        Setter for the width attribute.
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer.")
-        if value <= 0:
-            raise ValueError("Width must be > 0.")
-        self.__width = value
-
-    @property
-    def height(self):
-        """
-        Getter for the height attribute.
-        """
-        return (self.__height)
-
-    @height.setter
-    def height(self, value):
-        """
-        Setter for the height attribute.
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer.")
-        if value <= 0:
-            raise ValueError("Height must be > 0.")
-        self.__height = value
-
-    @property
-    def x(self):
-        """
-        Getter for the x attribute.
-        """
-        return (self.__x)
-
-    @x.setter
-    def x(self, value):
-        """
-        Setter for the x attribute.
-        """
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer.")
-        if value < 0:
-            raise ValueError("x must be >= 0.")
-        self.__x = value
-
-    @property
-    def y(self):
-        """
-        Getter for the y attribute.
-        """
-        return (self.__y)
+    def __init__(self, id=None):
+        """ Initializes an instance of the Base class """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
